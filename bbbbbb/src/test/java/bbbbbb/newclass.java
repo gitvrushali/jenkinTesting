b0f2353ddf4d0c5e5029cccabd52261b731f7622
vrushali <vrushalipatil316@gmail.com>
2024-08-09 19:05:55 +0200

    my jenkins commit

---
+++ bbbbbb/src/test/java/bbbbbb/newclass.java
@@ -0,0 +1,19 @@
+package bbbbbb;
+
+public class newclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a=2;
+		int b=3;
+		int sum=a+b;
+		
+		
+		
+		System.out.println("The sum of two number is   "+ sum);
+
+	}
+
+}
